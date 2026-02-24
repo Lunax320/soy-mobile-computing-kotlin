64e428f57ab815501f3c437d13adfec7cbd53d6a
Eliana Pardo <elianapardo320@gmail.com>
2026-02-24 17:46:20 -0500

    feat: implement explore and music discovery screen

--- app/src/main/java/com/example/soymusicreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/soymusicreviewapp/MainActivity.kt
@@ -1,47 +1,50 @@
-package com.example.soymusicreviewapp
+package com.example.nuevomovil
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.soymusicreviewapp.ui.theme.SOYMusicReviewAppTheme
+import com.example.soymusicreviewapp.ui.screens.explore.ExploreScreen
+import com.example.soymusicreviewapp.ui.theme.CompMovilProyectoTheme
+import com.example.soymusicreviewapp.ui.utils.FooterScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SOYMusicReviewAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Scaffold(
+                bottomBar = {
+                    FooterScreen()
                 }
+            ){
+                ExploreScreen(
+                    Modifier.padding(it)
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun GreetingPreview() {
-    SOYMusicReviewAppTheme {
-        Greeting("Android")
+fun GreetingPreview2() {
+    CompMovilProyectoTheme {
+        Scaffold(
+            bottomBar = {
+                FooterScreen()
+            }
+        ){
+            ExploreScreen(Modifier.padding(it))
+        }
     }
 }
