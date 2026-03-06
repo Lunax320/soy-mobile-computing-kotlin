554481db88d444d2b018f330b57d6118aeb7489d
Laura Lara <larala7789Qgmail.com>
2026-03-05 23:55:31 -0500

    style: Implementation of Material Theme 3 on SongDetailScreen

--- app/src/main/java/com/example/soymusicreviewapp/ui/screens/explore/SongsDetailScreen.kt
+++ app/src/main/java/com/example/soymusicreviewapp/ui/screens/explore/SongsDetailScreen.kt
@@ -6,15 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -54,7 +52,7 @@ fun SongsDetailScreenHeader(
                 Icon(
                     imageVector = Icons.Outlined.ArrowBack,
                     contentDescription = "Back",
-                    tint = Color.White
+                    tint = MaterialTheme.colorScheme.onPrimary
                 )
             }
             SongInfo(
